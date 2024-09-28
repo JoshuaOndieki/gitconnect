a52b6fdd28b67d153da2250b6762ec7b184c25e7
Joshua Ondieki <oj@joshuaondieki.com>
2024-09-28 03:44:22 +0300

    [Fix] window reference error

--- client/src/env.ts
+++ client/src/env.ts
@@ -3,7 +3,7 @@ export default {
         ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL : process.env.VERCEL_BRANCH_URL)
         ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_URL : process.env.VERCEL_URL)
         ?? process.env.NEXT_PUBLIC_METADATA_BASE
-        ?? typeof window !== "undefined" ? window.location.origin : "http://localhost:3000",
+        ?? (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
 
     NEXT_PUBLIC_APPWRITE_HOST_URL: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
     NEXT_PUBLIC_APPWRITE_PROJECT_ID: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
