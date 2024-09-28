f768e5f8977ef355ee30a9c50581645c3b9e516d
Joshua Ondieki <oj@joshuaondieki.com>
2024-09-28 04:24:41 +0300

    add http prefix to base url

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,9 +1,5 @@
 export default {
-    NEXT_PUBLIC_BASE_URL: (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL : process.env.VERCEL_PROJECT_PRODUCTION_URL)
-        ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL : process.env.VERCEL_BRANCH_URL)
-        ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_URL : process.env.VERCEL_URL)
-        ?? process.env.NEXT_PUBLIC_METADATA_BASE
-        ?? (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
+    NEXT_PUBLIC_BASE_URL: getUrl(),
 
     NEXT_PUBLIC_APPWRITE_HOST_URL: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
     NEXT_PUBLIC_APPWRITE_PROJECT_ID: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
@@ -13,4 +9,14 @@ export default {
         USERS: String(process.env.NEXT_PUBLIC_APPWRITE_FUNCTION_USERS),
         POSTS: String(process.env.NEXT_PUBLIC_APPWRITE_FUNCTION_POSTS),
     }
+}
+
+function getUrl() {
+    const url = (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL : process.env.VERCEL_PROJECT_PRODUCTION_URL)
+    ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL : process.env.VERCEL_BRANCH_URL)
+    ?? (typeof window !== "undefined" ? process.env.NEXT_PUBLIC_VERCEL_URL : process.env.VERCEL_URL)
+    ?? process.env.NEXT_PUBLIC_METADATA_BASE
+    ?? (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000")
+
+    return new URL(url.startsWith('http') ? url : `https://${url}`)
 }
